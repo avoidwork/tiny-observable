ba6724a64b3bb4becd457475a674ac9573ab8767
Jason Mulligan <jason.mulligan@avoidwork.com>
2023-11-05 12:08:32 -0500

    Updating README.md, removing unneeded constants

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,3 @@ export const INVALID_ARGUMENTS = "Invalid arguments";
 export const POSSIBLE_MEMORY_LEAK = "`Possible memory leak, more than {{LIMIT}} listeners for event: {{EVENT}}";
 export const TOKEN_EVENT = "{{EVENT}}";
 export const TOKEN_LIMIT = "{{LIMIT}}";
-export const UNDEFINED = "undefined";
-export const ZERO = "0";
